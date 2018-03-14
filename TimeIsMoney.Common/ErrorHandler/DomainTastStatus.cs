ab8b1830850980c13ec62718c2ddee60115a08a4
Harbaruk <grishagarbaruk1995@gmail.com>
2018-03-14 11:10:07 +0200

    Error Handler

---
+++ TimeIsMoney.Common/ErrorHandler/DomainTastStatus.cs
@@ -0,0 +1,17 @@
+﻿using TimeIsMoney.Common.ErrorHandler;
+
+namespace TimeIsMoney.Common
+{
+    public class DomainTaskStatus
+    {
+        private ErrorCollection _errorCollection = new ErrorCollection();
+
+        public ErrorCollection ErrorCollection => _errorCollection;
+
+        public bool HasErrors => _errorCollection.HasErrors;
+
+        public void AddError(string key, string error) => _errorCollection.AddError(key, error);
+
+        public void AddUnkeyedError(string error) => AddError("general", error);
+    }
+}
---
+++ TimeIsMoney.Common/ErrorHandler/ErrorCollection.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TimeIsMoney.Common.ErrorHandler
+{
+    public sealed class ErrorCollection
+    {
+        [JsonProperty("errors")]
+        private Dictionary<string, List<string>> _errors { get; set; } = new Dictionary<string, List<string>>();
+
+        [JsonIgnore]
+        public bool HasErrors => _errors.Any();
+
+        public void AddError(string key, string error)
+        {
+            if (!_errors.TryGetValue(key, out List<string> errors))
+            {
+                errors = new List<string>();
+                _errors[key] = errors;
+            }
+            errors.Add(error);
+        }
+    }
+}
